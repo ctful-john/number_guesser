f0ab1e0fd7d97d9069882bbc8df2ef4246d23996
John Doe <john.doe@ctful.ca>
2023-03-10 09:45:07 -0500

    First commit

---
+++ main.py
@@ -0,0 +1,23 @@
+import random
+
+number = random.randint(0, 1000)
+found = False
+
+print("Vous devez trouver un nombre aléatoire entre 0 et 1000")
+
+while not found:
+    print("Entez un nombre : ")
+    x = int(input())
+
+    if x == number:
+        found = True
+    elif x < number:
+        print("Le nombre doit être supérieur")
+    else:
+        print("Le nombre doit être inférieur")
+
+
+print("Bravo! Vous avez trouvé le nombre.")
+print("Voici votre flag : ")
+f = open("flag.txt", "r")
+print(f.read())
